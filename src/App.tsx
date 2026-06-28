8fda293b62cd1f5c58aaea2d376fdd5f7c82d4e2
codereen <zoyafreen04@gmail.com>
2026-06-28 13:56:35 +1000

    chore: configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
+/*function App() {
   return (
     <div>
       <h1>Prism Living</h1>
       <p>The world inspires. You decide.</p>
+    </div>
+  );
+}
+
+export default App;*/
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Prism Living
+      </h1>
     </div>
   );
 }
